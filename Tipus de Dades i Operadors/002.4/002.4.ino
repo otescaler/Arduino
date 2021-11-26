c71567e377338b753487c6d7e35286d7583e697e
otescaler <94806395+otescaler@users.noreply.github.com>
2021-11-26 16:43:55 +0100

    Create 002.4.ino

---
+++ Tipus de Dades i Operadors/002.4/002.4.ino
@@ -0,0 +1,48 @@
+  
+/*********************************************************************************
+**                                                                              **
+**                             TÍTOL:                                           **
+**              Posar un nom representatiu del programa                         **
+** NOM:Ot Escaler                                             DATA: XX/XX/20XX  **
+*********************************************************************************/
+//********* Includes *************************************************************
+
+//********* Variables ************************************************************
+int a = 5, b = 10, c = 20;
+
+//********* Setup ****************************************************************
+void setup()              // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+
+  Serial.println("Here is some math: ");
+
+  Serial.print("a = ");
+  Serial.println(a);
+  Serial.print("b = ");
+  Serial.println(b);
+  Serial.print("c = ");
+  Serial.println(c);
+
+  Serial.print("a + b = ");       // add
+  Serial.println(a + b);
+
+  Serial.print("a * c = ");       // multiply
+  Serial.println(a * c);
+  
+  Serial.print("c / b = ");       // divide
+  Serial.println(c / b);
+
+  Serial.print("c % b = ");       // module
+  Serial.println(c % b);
+  
+  Serial.print("b - c = ");       // subtract
+  Serial.println(b - c);
+}
+
+//********* Loop *****************************************************************
+void loop()  // we need this to be here even though its empty
+{
+}
+
+//********* Funcions *************************************************************
